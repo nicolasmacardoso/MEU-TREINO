1a005aa8e2169d15a0d493412f2a335ac25b73fd
Nícolas Machado Cardoso <nicolasxzx1225@gmail.com>
2026-04-16 22:47:11 -0300

    Estudando eventos e estados no React

--- meu-treino/src/App.tsx
+++ meu-treino/src/App.tsx
@@ -1,6 +1,23 @@
+import { useState } from "react";
+
 function App() {
+  const [isTraining, setIsTraining] = useState(false);
+  const [treinosFeitos, setTreinosFeitos] = useState(0);
+
+  function handleTrainButton() {
+    setIsTraining(prev => !prev);
+    !isTraining && setTreinosFeitos(prev => prev + 1);
+  }
+
   return (
     <div>
+      <h1>Meu treino</h1>
+      {treinosFeitos > 0 && <h2>Treinos feitos: {treinosFeitos}</h2>}
+
+      <p>{isTraining ? "Treino em andamento" : "Você não está treinando"}</p>
+      <button onClick={handleTrainButton}>
+        {isTraining ? "Parar" : "Treinar"}
+      </button>
     </div>
   );
 }
